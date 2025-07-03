e531e4cbcb7916d0a693230a175dbbb4c4a49603
aleksandravracaric <aleksandravracaric997@gmail.com>
2025-07-04 01:12:54 +0200

    fetched and displayed products

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -1,7 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getProducts } from '../network/ProductService'
 
 export default function ProductsPage() {
+    const [products, setProducts] = useState([])
+    const [category, setCategory] = useState('')
+    const [search, setSearch] = useState('')
+
+    useEffect(() => {
+        async function product() {
+            try {
+                const response = await getProducts({ category, search })
+                setProducts(response.data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        product()
+    }, [category, search])
+
     return (
-        <div>ProductsPage</div>
+        <div className='loginPage'>
+            <div className='headerContainerLogin'>
+                <h3>KONOVO</h3>
+                <input
+                    className='searchProductsInput'
+                    type="text"
+                    placeholder="Pretraži proizvod..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+            </div>
+
+
+            <div>
+                <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                    <option value="">Sve kategorije</option>
+                    <option value="Laptopovi i oprema">Laptopovi i oprema</option>
+                    <option value="Računarske komponente">Računarske komponente</option>
+                    <option value="Eksterni punjači, adapteri i baterije">Punjači</option>
+                </select>
+
+
+
+            </div>
+
+            <div className='cardProductsContainer'>
+                {products.map((product, index) => (
+                    <div key={index} class="productCard" >
+                        <img src={product.imgsrc} alt={product.naziv} width="100" />
+                        <div class="card-body">
+                            <h5 class="card-title"><strong>{product.naziv}</strong> – {product.price} RSD</h5>
+                            <p class="card-text"></p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
+
 }
