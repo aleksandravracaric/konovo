4071775b87671829b296e651887e9fe7a3ec0f49
aleksandravracaric <aleksandravracaric997@gmail.com>
2025-07-03 17:25:26 +0200

    changed base url for login

--- src/network/AuthService.js
+++ src/network/AuthService.js
@@ -1,10 +1,19 @@
 import axios from "axios"
 
-const BASE_URL = 'https://zadatak.konovo.rs'
+const BASE_URL = 'https://konovo.onrender.com'
+// https://zadatak.konovo.rs/login -> CORS nije podesen
+const apiKey = process.env.REACT_APP_API_KEY
+
+const headers = {
+    'Content-Type': 'application/json',
+    'x-api-key': `${apiKey}`,
+}
 
 export function login(username, password) {
-    return axios.post(`${BASE_URL}/login`, {
+    return axios.post(`${BASE_URL}/api/login`, {
         username: username,
         password: password
+    }, {
+        headers: headers
     })
 }
