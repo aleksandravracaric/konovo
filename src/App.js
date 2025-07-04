c085cadb0799a5f221c423251a2974ba530e2f4e
aleksandravracaric <aleksandravracaric997@gmail.com>
2025-07-04 17:44:55 +0200

    added product detail page and route path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import ProductsPage from './components/ProductsPage';
+import ProductDetailPage from './components/ProductDetailPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/login' element={<LoginPage />} />
           <Route path='/' element={<ProductsPage />} />
+          <Route path='/product/:sku' element={<ProductDetailPage />} />
         </Routes>
       </BrowserRouter>
     </>
