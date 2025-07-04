e83152aca257d2b32c94886cffe8f5a2748c8647
aleksandravracaric <aleksandravracaric997@gmail.com>
2025-07-04 15:35:10 +0200

    improved login loading ui

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -59,7 +59,7 @@ export default function LoginPage() {
                         </div>
                         <button type="submit" className="buttonLogin">
                             {loading ? (
-                                <div className="spinner-border spinner-border-sm-custom" role="status">
+                                <div className="spinner-border spinnerBorderCustom" role="status">
                                     <span className="visually-hidden">Loading...</span>
                                 </div>
                             ) : (
